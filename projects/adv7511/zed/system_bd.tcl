84e7bbefaa0a1a351b23743754191931d37a63b2
Sergiu Arpadi <sergiu.arpadi@analog.com>
2019-05-08 15:02:33 +0100

    system_id: added axi_sysid ip core
    added mem_init file generation to adi_board.tcl; added system id to adv7511/zed

--- projects/adv7511/zed/system_bd.tcl
+++ projects/adv7511/zed/system_bd.tcl
@@ -1,3 +1,13 @@
 
 source $ad_hdl_dir/projects/common/zed/zed_system_bd.tcl
 
+ad_ip_instance axi_sysid axi_sysid_0
+
+#generating memory init file
+sysid_gen_init_file
+
+# adding file path to ip
+ad_ip_parameter axi_sysid_0 CONFIG.PATH_TO_FILE "[pwd]/mem_init.txt"
+
+ad_ip_parameter axi_sysid_0 CONFIG.ROM_ADDR_BITS 6
+ad_cpu_interconnect 0x45000000 axi_sysid_0
